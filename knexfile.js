93a441cb9b8e18946319152b602fcac3ce9e2baf
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-12-18 10:07:15 +0100

    knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,30 +1,21 @@
-// Update with your config settings.
+const sharedConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  migrations: { directory: './data/migrations' },
+  seeds: { directory: './data/seeds' },
+};
 
 module.exports = {
   development: {
-    client: 'sqlite3',
+    ...sharedConfig,
     connection: {
       filename: './data/hobbits.db3',
     },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './data/migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
-    },
   },
   testing: {
-    client: 'sqlite3',
+    ...sharedConfig,
     connection: {
       filename: './data/test.db3',
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: './data/migrations',
-    },
-    seeds: {
-      directory: './data/seeds',
     },
   },
 };
