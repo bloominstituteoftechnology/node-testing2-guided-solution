2332375afea2176e0f42a9b190601e9d9f4d51ed
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-05-20 15:43:44 +0200

    upgrade deps, cleanup

--- knexfile.js
+++ knexfile.js
@@ -1,30 +1,24 @@
+const common = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  migrations: { directory: './data/migrations' },
+  seeds: { directory: './data/seeds' },
+}
+
 module.exports = {
   development: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    migrations: { directory: './data/migrations' },
-    seeds: { directory: './data/seeds' },
+    ...common,
     connection: {
       filename: './data/hobbits.db3',
     },
   },
   testing: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    migrations: { directory: './data/migrations' },
-    seeds: { directory: './data/seeds' },
+    ...common,
     connection: {
       filename: './data/test.db3',
     },
   },
   production: {
-    client: "pg",
-    connection: process.env.DATABASE_URL || "postgresql://postgres@localhost/hobbits",
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: { directory: "./data/migrations" },
-    seeds: { directory: "./data/seeds" },
+
   },
 };
